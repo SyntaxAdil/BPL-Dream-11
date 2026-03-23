119565ba6b054385292a9d3380d43e3bd1d8cd41
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-03-24 01:12:50 +0600

    Navbar complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar/Navbar";
+const App = () => {
+  return (
+    <>
+      <main className="max-w-6xl mx-auto my-2">
+        <Navbar />
+      </main>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,75 @@
+import NAV_IMAGE from "../../assets/logo.png";
+import COIN_IMAGE from "../../assets/DoubleDollar.png";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+const NAV_LINKS = [
+  { name: "Home", href: "/" },
+  { name: "Fixture", href: "/fixture" },
+  { name: "Teams", href: "/teams" },
+  { name: "Schedules", href: "/schedules" },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const links = NAV_LINKS.map((route) => (
+    <li key={route} onClick={() => setOpen((p) => !p)}>
+      <a
+        className="transition-colors duration-150 text-gray-600 hover:text-gray-900"
+        href={route.href}
+      >
+        {route.name}
+      </a>
+    </li>
+  ));
+
+  return (
+    <>
+      <nav className="p-4 flex items-center justify-between  px-6 ">
+        <div>
+          <img src={NAV_IMAGE} alt="LOGO" />
+        </div>
+        <ul className="hidden md:flex  items-center gap-10 font-medium ">
+          {links}
+          <button className="px-4 py-2 rounded-md border border-base-300 flex items-center gap-2 ">
+            0 Coin
+            <img src={COIN_IMAGE} alt="Double Dollar" />
+          </button>
+        </ul>
+
+        <button
+          className="btn btn-success btn-ghost block md:hidden"
+          onClick={() => setOpen((p) => !p)}
+        >
+          <Menu />
+        </button>
+      </nav>
+      <section
+        className={`fixed min-h-screen w-full backdrop-blur-[2px] inset-0 block md:hidden   ${open ? "translate-x-0" : "translate-x-1000"}`}
+        onClick={() => setOpen((p) => !p)}
+      ></section>
+      <aside
+        className={`max-w-md w-full bg-white h-full shadow p-6 fixed  z-10 top-0 right-0 block md:hidden transition-all duration-300 ease-in-out  ${open ? "translate-x-0" : "translate-x-full"}`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="flex items-center justify-between">
+          <img src={NAV_IMAGE} alt="LOGO" width={60} />
+          <button
+            className="btn btn-success btn-ghost"
+            onClick={() => setOpen((p) => !p)}
+          >
+            <X />
+          </button>
+        </div>
+        <ul className="flex flex-col mt-10 items-center  gap-8 font-medium px-2">
+          {links}
+          <button className="px-4 py-2 rounded-md border border-base-300 flex items-center justify-center gap-2 ">
+            0 Coin
+            <img src={COIN_IMAGE} alt="Double Dollar" />
+          </button>
+        </ul>
+      </aside>
+    </>
+  );
+};
+
+export default Navbar;
